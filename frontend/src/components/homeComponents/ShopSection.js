63be71df6f90fceba6884865f7c62ba10da06689
hongquangdao <79567305+hongquangdao@users.noreply.github.com>
2022-05-26 08:36:15 +0700

    pagination

--- frontend/src/components/homeComponents/ShopSection.js
+++ frontend/src/components/homeComponents/ShopSection.js
@@ -25,6 +25,63 @@ const ShopSection = (props) => {
     <>
       <div className="container">
         <div className="section">
+          <div className="section-title">
+            <h1 >Sản phẩm nổi bật </h1>
+          </div>
+          <div className="row">
+            <div className="col-lg-12 col-md-12 article">
+              <div className="shopcontainer row">
+                {
+                  loading ? (
+                    <div className="mb-5">
+                      <Loading />
+                    </div>
+                  ) : error ? (
+                    <Message variant="alert-danger" >{error}</Message>
+                  )
+                    : (
+                      <>
+                        {products.map((product) => (
+                          <div
+                            className="shop col-lg-3 col-md-6 col-sm-6"
+                            key={product._id}
+                          >
+                            <div className="border-product">
+                              <Link to={`/products/${product._id}`}>
+                                <div className="shopBack">
+                                  <img src={product.image} alt={product.name} />
+                                </div>
+                              </Link>
+
+                              <div className="shoptext">
+                                <p>
+                                  <Link to={`/products/${product._id}`}>
+                                    {product.name}
+                                  </Link>
+                                </p>
+
+                                <Rating
+                                  value={product.rating}
+                                  text={`${product.numReviews} reviews`}
+                                />
+                                <h3>${product.price}</h3>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                      </>
+                    )
+                }
+                {/* Pagination */}
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section">
+          <div className="section-title">
+            <h1 >Sản phẩm nổi bật </h1>
+          </div>
           <div className="row">
             <div className="col-lg-12 col-md-12 article">
               <div className="shopcontainer row">
